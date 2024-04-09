d3a3703031788b8f74ff3264a72c28a9e1b836e7
gabrielclf <gabrieldclf@gmail.com>
2024-04-09 10:20:18 -0300

    Começando lista4- estruturas de repetição

---
+++ ListaRepeticao/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ListaRepeticao
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //Q1
+      int a,b,soma;
+      soma = 0;
+
+      Console.WriteLine("Informe o primeiro número");
+      a = Convert.ToInt32(Console.ReadLine());
+
+      Console.WriteLine("Informe o segundo número");
+      b = Convert.ToInt32(Console.ReadLine());
+
+      for(int i = a+1;i<b;i++){
+        if (i % 2 != 0){
+            soma += i;
+            Console.WriteLine(i);
+        }
+      }
+    
+    Console.WriteLine($"A soma entre todos os números impares entre {a} e {b} é {soma}");
+    }
+  }
+}
