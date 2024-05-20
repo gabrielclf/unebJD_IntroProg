5fe9c6e2a1b7b11659cbb0035d8d8cd0ac57f7c5
gabrielclf <gabrieldclf@gmail.com>
2024-05-20 15:59:54 -0300

    preferi adicionar get e set manual para manipulação

--- Lab1/SempreEmForma.cs
+++ Lab1/SempreEmForma.cs
@@ -6,18 +6,20 @@ namespace LaboratorioUm
     {
         
         /*TODO - Requisitos*/
-        private int Peso { get; set;}
-        private double Altura { get;set;}
-        private string Nome {get;set;}
+        private int Peso ;
+        private double Altura ;
+        private string Nome;
         /*TODO - Construtor*/
         public Pessoa (int peso, double altura,string nome){
-            this.Peso = peso;
-            this.Altura = altura;
+            Peso = peso;
+            Altura = altura;
             Nome = nome;
         }
         /*TODO - Métodos*/
+
+
         public double calcularIMC(){
-            double imc = Peso*(Altura*Altura);
+            double imc = Peso/(Altura*Altura);
             return imc;
         }
 
@@ -34,7 +36,24 @@ namespace LaboratorioUm
             }
             return avaliacao;
         }
-
+        public int getPeso(){
+            return this.Peso;
+        }
+        public void setPeso(int peso){
+            this.Peso = peso;
+        }
+        public double getAltura(){
+            return this.Altura;
+        }
+        public void setAltura(double altura){
+            this.Altura = altura;
+        }
+        public string getNome(){
+            return this.Nome;
+        }
+        public void setNome(string nome){
+            this.Nome = nome;
+        }
         public void aumentarPeso(int n){
             Peso += n;
         }
@@ -45,22 +64,31 @@ namespace LaboratorioUm
 
         public static void Main(string[] args)
         {
-            /*Pessoa p1 = new Pessoa(62,1.55,"Numero_1");
+            Pessoa p1 = new Pessoa(62,1.55,"Numero_1");
             double imc = p1.calcularIMC();
-            Console.WriteLine($"IMC da pessoa é: {imc}");
+            /*Console.WriteLine($"IMC da pessoa é: {imc}");
             Console.WriteLine($"O peso da pessoa é {p1.Peso} Kg");
             Console.WriteLine($"A altura da pessoa é {p1.Altura} m");
             Console.WriteLine($"O nome da pessoa é {p1.Nome} "); */
 
             Console.WriteLine("Informe o nome da pessoa");
             string n = Console.ReadLine();
+            p1.setNome(n);
             Console.WriteLine("Informe o peso da pessoa");
             int pe = Convert.ToInt32(Console.ReadLine());
+            p1.setPeso(pe);
             Console.WriteLine("Informe a altura da pessoa");
             double alt = Convert.ToDouble(Console.ReadLine());
-            Pessoa p2 = new Pessoa(pe,alt,n);
-            string resultado_forma = p2.avaliacaoFisica();
-            Console.WriteLine($"O resultado da avaliação física é de IMC: {resultado_forma}");
+            p1.setAltura(alt);
+
+            Console.WriteLine($"IMC da pessoa é: {imc}");
+            Console.WriteLine($"O peso da pessoa é {p1.Peso} Kg");
+            Console.WriteLine($"A altura da pessoa é {p1.Altura} m");
+            Console.WriteLine($"O nome da pessoa é {p1.Nome} ");
+            
+            //Pessoa p2 = new Pessoa(pe,alt,n);
+            /*string resultado_forma = p2.avaliacaoFisica();
+            Console.WriteLine($"O resultado da avaliação física é de IMC: {resultado_forma}");*/
 
             
 
